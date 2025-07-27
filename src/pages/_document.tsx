cb7ecd69d86c4196dacc8cc9c294221bc6d62408
caruazu <gustavocaruazu@gmail.com>
2025-07-27 20:12:07 -0300

    desativando tema escuro e o básico de SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <meta
+          name="description"
+          content="Divulgação dos trabalhos do desenvolvedor caruazu."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
