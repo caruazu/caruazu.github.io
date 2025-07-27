8893a80b2e2f2ba714ba239672fb0ea75ccbcf6e
caruazu <gustavocaruazu@gmail.com>
2025-07-27 19:35:28 -0300

    bugfix, raiz do projeto

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/caruazu.github.io",
+  assetPrefix: "/caruazu.github.io/",
   output: "export", // <=== enables static exports
   images: { unoptimized: true },
   reactStrictMode: true,
