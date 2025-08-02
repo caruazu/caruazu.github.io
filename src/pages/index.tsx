bcf7623eabb9257121b19cc92c58e39f84fc590d
Gustavo Caruazu <39807202+caruazu@users.noreply.github.com>
2025-08-02 17:22:17 -0300

    Revert "nova versão com estilização"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -13,16 +14,83 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid  items-center justify-items-center gap-16 p-4 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20`}
+      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
-      <div className="max-w-sm min-w-sm rounded overflow-hidden bg-white">
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Gustavo Caruazu</div>
-          <p className="text-gray-700 text-base">
-            Oi, me chamo Gustavo, desenvolvo aplicações WEB.
-          </p>
-        </div>
-      </div>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>CARUAZU</h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://wa.me/message/Y7VNNQW6QXP2K1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/whatsapp.svg"
+            alt="File icon"
+            width={32}
+            height={32}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://www.instagram.com/caruazu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/instagram.svg"
+            alt="File icon"
+            width={32}
+            height={32}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://www.linkedin.com/in/caruazu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/linkedin.svg"
+            alt="File icon"
+            width={32}
+            height={32}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/caruazu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/github.svg"
+            alt="File icon"
+            width={32}
+            height={32}
+          />
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="mailto:gustavo@caruazu.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/mail.svg"
+            alt="File icon"
+            width={32}
+            height={32}
+          />
+        </a>
+      </footer>
     </div>
   );
 }
