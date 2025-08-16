16b0c22a36f3e55a2b0f6ff1e6bf69608747a6c3
caruazu <gustavocaruazu@gmail.com>
2025-08-16 16:11:59 -0300

    ajustes no rodapé e cabeçalho

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,76 +6,69 @@ export const Footer = () => {
   return (
     <footer className="bg-slate-dark ">
       <div className="w-full max-w-7xl mx-auto p-6">
-        <div className="row-start-3 flex justify-between items-center">
-          {/* Links à direita */}
-          <div className="flex gap-4">
-            <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://www.instagram.com/healthchain-brasil/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/instagram.svg"
-                alt="Instagram"
-                width={32}
-                height={32}
-                style={{ filter: "invert(100%)" }}
-              />
-            </a>
-            <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="linkedin.com/company/healthchain-brasil/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/linkedin.svg"
-                alt="LinkedIn"
-                width={32}
-                height={32}
-                style={{ filter: "invert(100%)" }}
-              />
-            </a>
+        <div className="text-1xl font-bold text-center text-white">
+          <Link href="#header">HEALTHCHAIN | BRASIL</Link>
+        </div>
+
+        <div className="flex justify-center p-3 gap-4">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://www.instagram.com/healthchain-brasil/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/instagram.svg"
+              alt="Instagram"
+              width={32}
+              height={32}
+              style={{ filter: "invert(100%)" }}
+            />
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://www.linkedin.com/company/healthchain-brasil/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/linkedin.svg"
+              alt="LinkedIn"
+              width={32}
+              height={32}
+              style={{ filter: "invert(100%)" }}
+            />
+          </a>
+        </div>
+
+        <div className="w-full max-w-7xl mx-auto flex flex-col items-center text-center ">
+          <div className="text-white text-xs font-bold pb-3">
+            <p className="flex items-center gap-2 ">
+              <Mail className="w-4 h-4" />
+              <a
+                className="hover:underline"
+                href={`mailto:future@healthchain.tec.br`}
+              >
+                future@healthchain.tec.br
+              </a>
+            </p>
           </div>
 
-          {/* Nome à esquerda */}
-
-          <div className="text-1xl font-bold text-white">
-            <Link href="#header">HEALTHCHAIN</Link>
+          <div className="text-white text-xs font-bold">
+            <p className="flex items-center gap-2">
+              <Phone className="w-4 h-4" />
+              <a className="hover:underline" href={`tel:+55 82 3025-0497`}>
+                {"+55 82 3025-0497"}
+              </a>
+            </p>
           </div>
         </div>
-      </div>
 
-      <div className="w-full max-w-7xl mx-auto pl-6 pb-3">
-        {/* Links à direita */}
-        <div className="text-white text-xs font-bold pb-3">
-          <p className="flex items-center gap-2 ">
-            <Mail className="w-4 h-4" />
-            <a
-              className="hover:underline"
-              href={`mailto:future@healthchain.tec.br`}
-            >
-              future@healthchain.tec.br
-            </a>
-          </p>
+        <div className="text-gray-500 text-xs text-center pt-2">
+          <p>CNPJ 53.351.091/0001-76</p>
         </div>
-
-        {/* Nome à esquerda */}
-        <div className="text-white text-xs font-bold">
-          <p className="flex items-center gap-2">
-            <Phone className="w-4 h-4" />
-            <a className="hover:underline" href={`tel:+55 82 3025-0497`}>
-              {"+55 82 3025-0497"}
-            </a>
-          </p>
-        </div>
-      </div>
-
-      <div className="text-gray-500 text-xs text-center pb-1">
-        <p>© 2025 Healthchain. CNPJ 53.351.091/0001-76</p>
       </div>
     </footer>
   );
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,16 +4,18 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header id="header" className="bg-forest-light">
-      <div className="container flex items-center justify-between  w-full max-w-7xl mx-auto p-6">
+      <div className="container flex items-center justify-between  w-full max-w-7xl mx-auto p-4">
         <div className="text-1xl font-bold text-gray-800">
           <Link href={"/"}>HEALTHCHAIN</Link>
         </div>
 
-        <nav className="flex items-center space-x-6">
+        <nav className="flex items-center space-x-3 md:space-x-6 text-[12px] md:text-[14px]">
           <Link href={"#projetos"}>Projetos</Link>
           <Link href={"#planos"}>Preços</Link>
           <Link href="#contato">
-            <Button className="cursor-pointer">Contato</Button>
+            <Button className="p-2 text-[12px] md:text-[14px] md:p-4 cursor-pointer">
+              Contato
+            </Button>
           </Link>
         </nav>
       </div>
